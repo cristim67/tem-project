3a2fb3590becee6f2de1eceabf09bffe1b2d291b
Cristi Miloiu <miloiuc4@gmail.com>
2026-01-16 11:58:20 +0200

    update env

--- server/app/core/database.py
+++ server/app/core/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+import dotenv
 
-DATABASE_URL = ""
+dotenv.load_dotenv()
+
+DATABASE_URL = os.getenv("SQL_DATABASE")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
